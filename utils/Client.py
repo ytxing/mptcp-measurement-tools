6a87143ce9103de453ea761d30c277bc2223f33d
ytxing <ytxing96@qq.com>
2022-01-11 12:47:50 +0800

    bug fixed

--- utils/Client.py
+++ utils/Client.py
@@ -7,26 +7,8 @@ import time
 import types
 from mytools import *
 class Client(ExpNode):
-    def __init__(self, id: str, connection: socket.socket, reqTrunkSize, trunkSize, round, role:str, type:str, wait4Reply=True, waitTimer=10.0):
-        self.id: str = id
-        self.connection: socket.socket = connection
-        self.reqTrunkSize = reqTrunkSize # need to be less than 0xFFFFFFFF = 4294967295 ~= 4.2GB
-        self.trunkSize = trunkSize
-        self.round = round
-        self.wait4Reply = wait4Reply
-        self.waitTimer = waitTimer
-        self.role = role
-        self.type = type
-        self.typeCode: int = getExpType(role, type)
-
-        
-        self.sendingThread = None
-        self.recvingThread = None
-        self.sendQueue = Queue(0)
-        self.queueLock = threading.Lock()
-        self.sendingThreadEnd = False
-        self.recvingThreadEnd = False
-        self.forceQuit = False # 不知道有没有用，用来强行停止线程
+    def __init__(self, id: str, connection: socket.socket, type:str, wait4Reply=True, waitTimer=10.0):
+        ExpNode.__init__(self, id, connection=connection, role='c', type=type, wait4Reply=wait4Reply, waitTimer=waitTimer)
 
     def recvData(self):
         # Message格式: [(good) (end) (random string)]
@@ -38,7 +20,7 @@ class Client(ExpNode):
             recvdStr = self.connection.recv(APP_READ_BUFFER_SIZE).decode(ENCODING)
             bufferedStr += recvdStr
             bytesRecved += len(recvdStr)
-            bufferedStr = self.queueAllMsg(bufferedStr)
+            bufferedStr = self.putAllMsgInRecvQueue(bufferedStr)
 
             MSoMPrint('ID:{} bytesRecved + {} = {}'.format(self.id, len(recvdStr), bytesRecved))
             # if bytesRecved >= self.trunkSize * self.round:
@@ -47,39 +29,26 @@ class Client(ExpNode):
             # ytxing: TODO 这里就可以通过处理这个trunk获得各种信息 
 
         MSoMPrint('ID:{} stop recving data Force:{} End:{} bytesRecved:{}'.format(self.id, self.forceQuit, self.recvingThreadEnd, bytesRecved))
-    
-    def sendData(self):
-        MSoMPrint('ID:{} sendData start sending data'.format(self.id))
-        self.sendingThreadEnd = False
-        while not self.forceQuit and not self.sendingThreadEnd:
-            try:
-                msg: Message = self.getMsgFromQueue()
-                trunk = msg.trunk
-                MSoMPrint('ID:{} sendData sending a trunk len:{}'.format(self.id, len(trunk)))
-                self.connection.sendall(trunk.encode(ENCODING))
-                MSoMPrint('ID:{} sendData sent a trunk len:{}'.format(self.id, len(trunk)))
-                if msg.end:
-                    self.sendingThreadEnd = 1
-            except: 
-                MSoMPrint('ID:{} sendData nothing to send in queue timeout, continue'.format(self.id))
 
-        MSoMPrint('ID:{} sendData stop sending data Force:{} End:{}'.format(self.id, self.forceQuit, self.sendingThreadEnd))
 
     def start(self):
-        # 首先发送初始request给服务器，如有需要继续发送其他的
-
-        # TODO 创建并开启两个线程，初始化queue
+        '''
+        对于每一种Application，继承Client类并重写start函数，按照应用的要求使用putMsgInQueue()和getMsgFromQueue()来控制数据的发送和接收。
+        '''
         self.recvingThread = threading.Thread(target=self.recvData)
         self.sendingThread = threading.Thread(target=self.sendData)
         self.sendingThread.start()
         self.recvingThread.start()
 
-        msg = Message(1, 0, self.typeCode, reqTrunkSize=100, size=self.trunkSize)
-        self.putMsgInQueue(msg)
-        msg = Message(1, 0, self.typeCode, reqTrunkSize=100, size=self.trunkSize)
-        self.putMsgInQueue(msg)
-        msg = Message(1, 1, self.typeCode, reqTrunkSize=114514, size=self.trunkSize)
-        self.putMsgInQueue(msg)
+        msg = Message(1, 0, self.typeCode, reqTrunkSize=100, size=1)
+        print(msg.trunk)
+        self.putMsgInSendQueue(msg)
+        msg = Message(1, 0, self.typeCode, reqTrunkSize=100, size=1)
+        print(msg.trunk)
+        self.putMsgInSendQueue(msg)
+        msg = Message(1, 1, self.typeCode, reqTrunkSize=114514, size=1)
+        print(msg.trunk)
+        self.putMsgInSendQueue(msg)
         MSoMPrint(self.id, 'putting msg in queue type:{} ctrl:{:08b}'.format(bin(msg.type), msg.ctrl))
         time.sleep(1)
 
@@ -115,5 +84,5 @@ sock.connect(server_address)
 
 # ytxing: TODO 这里客户端从collect_server获得当次实验的参数
 
-client = Client('test-client', connection=sock, trunkSize=10, round=1, role='c', type='bulk', reqTrunkSize=1200)
+client = Client('test-client', connection=sock, type='bulk')
 client.start()
