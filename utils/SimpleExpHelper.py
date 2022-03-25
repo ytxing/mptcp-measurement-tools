b5eeb6c377749a4184edccb75862fd1fbbcd03fd
ytxing <ytxing96@qq.com>
2022-03-25 11:10:07 +0800

    1

--- utils/SimpleExpHelper.py
+++ utils/SimpleExpHelper.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
                     print( p.stdout.read().decode().strip())
                     p.wait()
                     now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                    print("[{}] {} output {}".format(now, cmd_i, p.stdout.read().decode()).strip())
+                    print("[{}] {} \noutput {}".format(now, cmd_i, p.stdout.read().decode()).strip('\n'))
 
                 subprocess.call("echo infonet123 | sudo -S echo '[{}] changed algs' > /var/www/html/server_status.txt".format(now), shell=True)
                 subprocess.call("echo infonet123 | sudo -S sysctl net.mptcp.mptcp_scheduler >> /var/www/html/server_status.txt", shell=True)
