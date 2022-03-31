938d6a59541124e01230ec0576c104f6607a3e7e
ytxing <ytxing96@qq.com>
2022-03-31 12:10:57 +0800

    Player 用回threading而非multiprocess

--- utils/HttpClient.py
+++ utils/HttpClient.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import multiprocessing
+import threading
 import queue
 import subprocess
 import requests
@@ -50,6 +51,7 @@ class MimicPlayer:
         while self.played_seg_count < self.total_seg_count:
             try:
                 start = time.time()
+                print('qsize: ' + str(self.replay_buffer.qsize()))
                 seg = self.replay_buffer.get(timeout=60)
             except:
                 self.logger.log("queue timeout, something WRONG")
@@ -58,7 +60,7 @@ class MimicPlayer:
                 self.timer_pause += time.time() - start
                 self.logger.log("pasue for {:.4f}s".format(time.time() - start))
                 if seg is not None and seg == 4:
-                    self.logger.log("Playing... seg_No.(4s):{}".format(self.played_seg_count))
+                    self.logger.log("Playing... seg_No.({}s):{}".format(seg, self.played_seg_count))
                     time.sleep(seg)
                     self.played_seg_count += 1
 
@@ -86,7 +88,7 @@ class MimicPlayer:
         ```
         '''
         server_url = self.url
-        self.PlayerProcessing = multiprocessing.Process(target=self.Player)
+        self.PlayerProcessing = threading.Thread(target=self.Player)
         self.replay_buffer.put(4)
         self.got_seg_count += 1
         
@@ -107,6 +109,7 @@ class MimicPlayer:
                 tools.downloadFile('bbb_30fps_{}_4s_{}.m4v'.format(self.resolution, self.got_seg_count), '{}/stream/bbb_30fps_{}_4s.m4v'.format(server_url, self.resolution), s, logger=self.logger)
                 self.got_seg_count += 1
                 self.replay_buffer.put(4)
+                self.logger.log("qsize:{}".format(self.replay_buffer.qsize()))
         
         self.PlayerProcessing.join()
         self.logger.log("Final Result {} 1st_seg_time(s):{:.4f} all(s):{:.4f} pause(s):{:.4f}".format(self.resolution, self.timer_start, self.timer_all, self.timer_pause))
@@ -191,7 +194,13 @@ def startExperiment(url: str, type: str, log_path: str='./log/', log_file_name: 
     # dumpWlanProcess = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
     # cmd = 'echo a | sudo -S tcpdump -l -n -i {} src host {}'.format(nic_lte, '47.100.85.48')
 
-
+    cmd = 'echo a | sudo -S tcpdump -l -n -v -i {} src host {} and port 80'.format(nic_lte, url[7:])
+    logger.log(cmd)
+    dump_lte = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    cmd = 'echo a | sudo -S tcpdump -l -n -v -i {} src host {} and port 80'.format(nic_wlan, url[7:])
+    logger.log(cmd)
+    dump_wlan = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    time.sleep(1)
     # start the experiment
     s = requests.Session()
     if type == 'bulk':
@@ -217,6 +226,11 @@ def startExperiment(url: str, type: str, log_path: str='./log/', log_file_name: 
             GoStreamProcessing.terminate()
 
     s.close()
+    time.sleep(1)
+    lte_bytes_count = tools.getDumpedBytes(dump_lte.stdout)
+    wlan_bytes_count = tools.getDumpedBytes(dump_wlan.stdout)
+    logger.log('DUMP BYTES ifname:{} total(bytes):{} '.format(nic_lte, lte_bytes_count))
+    logger.log('DUMP BYTES ifname:{} total(bytes):{} '.format(nic_wlan, wlan_bytes_count))
     lte_bytes_end = tools.getRcvBytesOfIface(nic_lte)
     wlan_bytes_end = tools.getRcvBytesOfIface(nic_wlan)
     logger.log('NIC BYTES ifname:{} total(bytes):{} '.format(nic_lte, lte_bytes_end - lte_bytes_start))
